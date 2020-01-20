2df8a4119251465575d937aa005c677f15bb587d
Spencer Blum <spencerblum@gmail.com>
2020-01-20 22:57:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,10 +53,18 @@ def monopoly_with_fourth_tier
 				:four_pieces_owned => 200
       },
 			:names => {
-				:reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shoreline => {}
+				:reading_railroad => {
+          :mortgage_value => "$100"
+        },
+        :pennsylvania_railroad => {
+          :mortgage_value => "$200"
+        },
+        :b_and_o_railroad => {
+          :mortgage_value => "$400"
+        },
+        :shoreline => {
+          :mortgage_value => "$800"
+        }
 			}
 	}
 }
